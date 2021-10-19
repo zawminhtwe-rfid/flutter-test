4d0822c0c6eedd9e5f08443cd309f0534abe173f
zawminhtwe-rfid <zawminhtwe53@gmail.com>
2021-10-19 15:06:38 +0630

    1

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -60,7 +60,20 @@ class _HomePageState extends State<HomePage> {
           Card(
             child: ListTile(
               leading: Icon(Icons.star, color: Colors.blue),
-              title: Text('Master Test Test'),
+              title: Text('Flutter Test'),
+              subtitle: Text('All your favourite widgets'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CurveNavBar()));
+                print("click zoom drawer");
+              },
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.star, color: Colors.blue),
+              title: Text('Alpho'),
               subtitle: Text('All your favourite widgets'),
               trailing: Icon(Icons.arrow_forward_ios),
               onTap: () {
