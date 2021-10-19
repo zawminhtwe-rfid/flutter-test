0a9ffe2ee821b5f4beda0d4ef04cbbf0c1fd4685
zawminhtwe-rfid <zawminhtwe53@gmail.com>
2021-10-19 15:01:36 +0630

    change master

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -60,7 +60,7 @@ class _HomePageState extends State<HomePage> {
           Card(
             child: ListTile(
               leading: Icon(Icons.star, color: Colors.blue),
-              title: Text('Master Test'),
+              title: Text('Master Test Test'),
               subtitle: Text('All your favourite widgets'),
               trailing: Icon(Icons.arrow_forward_ios),
               onTap: () {
